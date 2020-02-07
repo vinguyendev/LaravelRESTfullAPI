5b50f936f0ebf25f56750a3151625b1a4dfa1fc7
Vi Nguyen <vinguyen.dev@gmai.com>
2020-02-07 09:07:35 +0700

    api passport

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Http;
 
 use App\Http\Middleware\AuthKey;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Laravel\Passport\Http\Middleware\CheckClientCredentials;
 
 class Kernel extends HttpKernel
 {
@@ -42,7 +43,7 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             'bindings',
 //            \App\Http\Middleware\AuthKey::class,
-            \App\Http\Middleware\AuthBasic::class,
+//            \App\Http\Middleware\AuthBasic::class,
 //            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
@@ -65,6 +66,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'client' => CheckClientCredentials::class,
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -22,5 +22,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 //Route::post('country','Country\CountryController@countrySave');
 //Route::put('country/{id}','Country\CountryController@countryUpdate');
 //Route::delete('country/{id}','Country\CountryController@countryDelete');
-
-Route::apiResource('country','Country\Country');
+Route::group(['middleware'=>'auth:api'],function (){
+    Route::apiResource('country','Country\Country')->middleware('client');
+});
